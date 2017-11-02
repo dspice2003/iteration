e15a583d3497039ba708f4e32f0cfcf02a543d25
dspice2003 <dspice@sjnma.org>
2017-11-02 16:27:37 -0500

    done with problem1

--- problem1.py
+++ problem1.py
@@ -1,1 +1,13 @@
-
+for numbers in range(1,1001):
+	if numbers % 3 == 0 :
+		print("burrito")
+	elif numbers % 5 == 0 :
+		print("cheeto")
+	elif numbers % 7 == 0 :
+		print("veto")
+	elif numbers % 3 == 0 and numbers % 5 == 0 :
+		print("burritoCheeto")
+	elif numbers % 5 == 0 and numbers % 7 == 0 :
+		print("burritoVeto")
+	elif numbers % 3 == 0 and numbers % 5 == 0  and numbers % 7 == 0 :
+		print("BurritoCheetoVeto")
